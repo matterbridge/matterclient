60758c9d48c0c72c07b8169cc2511dfebf08b980
Haw Loeung <haw.loeung@canonical.com>
2026-08-04 06:01:44 +1000

    Improve on WebSocket event loop making it consistent and zero-allocation (#48)
    * Improve on WebSocket event loop making it consistent and zero-allocation
    
    * Optimize digestString() used for de-duplication

--- messages.go
+++ messages.go
@@ -3,8 +3,8 @@ package matterclient
 import (
 	"context"
 	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"strconv"
 	"strings"
 
@@ -460,5 +460,10 @@ func (m *Client) parseMessage(rmsg *Message) {
 }
 
 func digestString(s string) string {
-	return fmt.Sprintf("%x", md5.Sum([]byte(s))) //nolint:gosec
+	if len(s) == 0 {
+		return ""
+	}
+
+	sum := md5.Sum([]byte(s)) //nolint:gosec
+	return hex.EncodeToString(sum[:])
 }
